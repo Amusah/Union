1fe52cc1c0f1439b0437128a88ddc45dfce71012
Amusah <henryamusah@gmail.com>
2024-04-18 22:36:17 +0000

    completed hero section

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const burger = document.querySelector(".burger");
 const arrow = document.querySelector(".arrow");
 const searchField = document.querySelector(".form");
 const banner = document.querySelector(".banner");
+const bannerBtn = document.querySelector(".banner-btn");
 
 /*********** NAVIGATION BAR ************/
 
@@ -48,11 +49,34 @@ const sliderImages = [
 
 let counter = 0;
 const changeImage = () => {
-  banner.style.transition = "4s ease-in";
+  banner.style.transition = "2s ease-in";
   banner.style.backgroundImage = `url(${sliderImages[counter]})`;
   counter++;
 
   if (counter === sliderImages.length) counter = 0;
 };
 
-setInterval(changeImage, 5000);
+setInterval(changeImage, 7000);
+
+
+bannerBtn.addEventListener("click", ({ target }) => {
+  // console.log('clicked')
+  if (target.classList.contains("btn__next") || target.classList.contains("right-arrow")) {
+    console.log('next')
+    if(counter === sliderImages.length - 1){
+      counter = 0;
+    } else{
+      counter++;
+    }
+    changeImage();
+  }
+
+  if(target.classList.contains('btn__prev') || target.classList.contains("left-arrow")){
+    if(counter === 0){
+      counter = sliderImages.length - 1;
+    } else{
+      counter--;
+    }
+    changeImage();
+  }  
+});
